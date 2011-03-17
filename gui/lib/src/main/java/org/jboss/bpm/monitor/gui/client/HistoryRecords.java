fadc68019bd3d8453fb0e7f262035083a69aeb82
Jeff Yu <jeff.yuchang@gmail.com>
2011-03-17 19:03:43 +1100

    * remove the getInstance(...) methods, as now we published it as a restful web service.

--- gui/lib/src/main/java/org/jboss/bpm/monitor/gui/client/HistoryRecords.java
+++ gui/lib/src/main/java/org/jboss/bpm/monitor/gui/client/HistoryRecords.java
@@ -26,7 +26,5 @@ public interface HistoryRecords
     Set<String> getFailedInstances(String definitionKey, long timestamp, String timespan);
     
     List<String> getAllEvents(String instanceKey);
-    
-    List<String> getInstances(String definitionKey, String status, long startTime, long endTime, String correlationKey);
-
+  
 }
